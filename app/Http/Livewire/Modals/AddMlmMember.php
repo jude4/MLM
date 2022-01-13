b1ac1ff233d8506423df6d72442b9ed4cf6de184
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-01-13 12:14:26 +0100

    add mlm member form

--- app/Http/Livewire/Modals/AddMlmMember.php
+++ app/Http/Livewire/Modals/AddMlmMember.php
@@ -2,10 +2,49 @@
 
 namespace App\Http\Livewire\Modals;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class AddMlmMember extends Component
 {
+    public $uuid;
+    public $email;
+    public $password;
+    public $password_confirmation;
+    public $nickname;
+
+    protected $rules = [
+        'uuid' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'nickname' => 'required|min:5|unique:users,nickname',
+        'password' => 'required|alpha_dash|min:8|confirmed',
+        'password_confirmation' => 'required',
+        // 'agreement' => 'required',
+    ];
+
+    public function mount()
+    {
+        $this->uuid = User::generateUUID();
+    }
+
+    public function addMember()
+    {
+        $this->validate();
+
+        $user = User::create([
+            'user_id' => $this->uuid,
+            'nickname' => $this->nickname,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        $user->giveParentById(Auth::user()->id);
+
+        return redirect()->route('user.pvmytree')->with('toast_success', 'New Member Added!');
+    }
+
     public function render()
     {
         return view('livewire.modals.add-mlm-member');
