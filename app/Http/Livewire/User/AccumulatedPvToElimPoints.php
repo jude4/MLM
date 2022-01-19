5b16da31e85b31943ad1e224d1c0efab861cb1bd
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-01-19 09:55:53 +0100

    setup pv to elim points pages on livewire

---
+++ app/Http/Livewire/User/AccumulatedPvToElimPoints.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class AccumulatedPvToElimPoints extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.accumulated-pv-to-elim-points');
+    }
+}
---
+++ resources/views/livewire/user/available-pv-to-elim-points.blade.php
@@ -0,0 +1,48 @@
+<div class="row justify-content-center">
+    <div class="col-lg-8 col-md-9 col-sm-10 col-12">
+        <div class="form-group row mt-3">
+            <label class="inp-pv col-xl-3 col-lg-12  col-md-12 col-12 pr-0 align-self-center">Conversion Quantity</label>
+            <input type="text" class="form-control pro-pv-input col-xl-7 col-lg-9 col-md-9 col-9 ml-2" placeholder="">
+           
+        </div>
+        <div class="row mb-4">
+            <div class="col-xl-3 col-lg-2 col-md-0 col-sm-5 col-0">
+            </div>
+            <div class="col-xl-7 col-lg-10 col-md-12 col-sm-10 col-12 px-0">
+                <ul class="rs-details">
+                    <li><a href="#" class="btn-rp-detail">500,000</a></li>
+                    <li><a href="#" class="btn-rp-detail">1 million</a></li>
+                    <li><a href="#" class="btn-rp-detail">2 million</a></li>
+                    <li> <a href="#" class="btn-rp-detail">10 million</a></li>                                   
+                </ul>
+            </div>
+        </div>
+        <div class="form-group row mr-sp">
+            <label class="inp-pv col-xl-3 col-lg-12  col-md-12 col-12 pr-0 align-self-center">Conversion Fee</label>
+           <input type="text" class="form-control pro-pv-inputs col-xl-7 col-lg-9 col-md-9 col-9 ml-2" placeholder="">
+            <div class="elim-point-text">PV</div>
+        </div>
+        <div class="form-group row mt-3">
+            <label class="inp-pv col-xl-3 col-lg-12  col-md-12 col-12 pr-0 align-self-center">Quantity after conversion</label>
+            <input type="text" class="form-control pro-pv-inputs col-xl-7 col-lg-9 col-md-9 col-9 ml-2" placeholder="">
+            <div class="elim-point-text">ELIM POINT</div>
+        </div>
+        <div class="form-group row mt-3">
+            <label class="inp-pv col-xl-3 col-lg-12  col-md-12 col-12 pr-0 align-self-center">Balance after conversion</label>
+            <input type="text" class="form-control pro-pv-inputs col-xl-7 col-lg-9 col-md-9 col-9 ml-2" placeholder="">
+            <div class="elim-point-text">PV</div>
+        </div>
+        <div class="row def-sp">
+            <div class="col-12">
+                <div class="pv-add-app-text">- Cancellation after conversion is not possible.</div>
+            </div>
+        </div>
+        <div class="row mt-3 justify-content-end">
+            <div class="col-3 text-center px-0">
+                 <a href="#" class="btn-max">Application</a>
+            </div>
+        </div>
+
+        <div class="mt-21"></div>
+    </div>
+</div>
