d1ff640b13932a79d8a2001819967187676e922e
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-01-27 12:17:47 +0100

    prepared migration for pursue trading

--- database/migrations/2022_01_24_232948_create_pursue_trades_table.php
+++ database/migrations/2022_01_24_232948_create_pursue_trades_table.php
@@ -17,6 +17,38 @@ class CreatePursueTradesTable extends Migration
             $table->id();
             // $table->foreignId('user_id')->constrained('users')
             $table->timestamps();
+            $table->foreignId('user_id')->onDelete('cascade');
+            $table->string('subject');
+            $table->boolean('status')->default(true);
+            $table->integer('state')->default(0);
+            $table->integer('opertaional_capital');
+            $table->float('percentage_yield')->nullable();
+            //purchase amounts
+            $table->integer('first_purchase_amount')->nullable();
+            $table->integer('second_purchase_amount')->nullable();
+            $table->integer('third_purchase_amount')->nullable();
+            $table->integer('fourth_purchase_amount')->nullable();
+            //purchase prices
+            $table->integer('first_purchase_price')->nullable();
+            $table->integer('second_purchase_price')->nullable();
+            $table->integer('third_purchase_price')->nullable();
+            $table->integer('fourth_purchase_price')->nullable();
+            // average purchase prices
+            $table->integer('first_av_purchase_price')->nullable();
+            $table->integer('second_av_purchase_price')->nullable();
+            $table->integer('third_av_purchase_price')->nullable();
+            $table->integer('fourth_av_purchase_price')->nullable();
+            //selling prices
+            $table->integer('first_selling_price')->nullable();
+            $table->integer('second_selling_price')->nullable();
+            $table->integer('third_selling_price')->nullable();
+            $table->integer('fourth_selling_price')->nullable();
+            //currency and segment indicators
+            $table->string('currency')->nullable();
+            $table->integer('segment_bought')->default(0);
+            $table->integer('segment_sold')->default(0);
+
+
         });
     }
 
