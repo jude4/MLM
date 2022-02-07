2185fe4c094eea2a1740979262e433408fc32e1e
unknown <sagarcodewexy@gmail.com>
2022-02-07 14:27:48 +0530

    two table migration create

---
+++ database/migrations/2022_02_07_084045_create_user_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('admin_id')->default(0);
+            $table->integer('permission_id')->default(0);
+            $table->tinyInteger('is_write')->default(0);
+            $table->tinyInteger('status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_permissions');
+    }
+}
